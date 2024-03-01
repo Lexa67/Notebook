fcb68d4988621ba374404b3a05387e32312d07c2
Lexa67 <3aj4ik504@gmail.com>
2024-03-01 20:37:39 +0200

    first two models

---
+++ db/migrate/20240301182640_create_lessons.rb
@@ -0,0 +1,10 @@
+class CreateLessons < ActiveRecord::Migration[7.0]
+  def change
+    create_table :lessons do |t|
+      t.datetime :lesson_date
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
